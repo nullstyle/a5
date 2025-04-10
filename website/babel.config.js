5da18dde3fb779ad2e33e34d41c8f5cca2c791f0
Felix Palmer <felixpalmer@gmail.com>
2025-04-10 18:11:21 +0200

    revert

--- website/babel.config.js
+++ website/babel.config.js
@@ -2,23 +2,10 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
-const {resolve} = require('path');
-
 module.exports = {
-  presets: ['@docusaurus/core/lib/babel/preset'],
+  presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
   plugins: [
     // Ensure consistently hashed component classNames between environments (a must for SSR)
-    'styled-components',
-    [
-      'module-resolver',
-      {
-        root: [resolve(__dirname, 'node_modules')],
-        alias: {
-          'website-examples': resolve(__dirname, '../examples/website'),
-          'a5': resolve(__dirname, '../modules/'),
-          'a5-internal': resolve(__dirname, '../modules/internal/')
-        }
-      }
-    ]
+    'styled-components'
   ]
 };
