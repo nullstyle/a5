8add5d394ee73a031a6285c17468d830e72c72f2
Felix Palmer <felixpalmer@gmail.com>
2025-05-13 23:00:15 +0200

    cjs & js

--- tsup.config.ts
+++ tsup.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   external: [/^internal/],
   outExtension({ format }) {
     return {
-      js: format === 'cjs' ? '.js' : '.mjs',
+      js: format === 'cjs' ? '.cjs' : '.js',
     }
   },
 }) 
