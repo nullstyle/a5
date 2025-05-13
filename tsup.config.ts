e520c3ee2a206b09cb5fe8296471f6e830841606
Felix Palmer <felixpalmer@gmail.com>
2025-05-13 08:04:06 +0200

    Switch to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['modules/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  external: [/^internal/],
+  outExtension({ format }) {
+    return {
+      js: format === 'cjs' ? '.js' : '.mjs',
+    }
+  },
+}) 
